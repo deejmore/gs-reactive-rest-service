2d119c8b9e56900b1f91a4dcfd3e273ad9e2186d
DJ Moralita <deejmore@me.com>
2018-10-02 17:08:51 +1000

    add new files

---
+++ initial/src/main/java/hello/GreetingRouter.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class GreetingRouter {
+
+	@Bean
+	public RouterFunction<ServerResponse> routeHello(GreetingHandler greetingHandler) {
+		return RouterFunctions
+			.route(RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), greetingHandler::hello);
+	}
+	
+	@Bean
+	public RouterFunction<ServerResponse> routeHola(GreetingHandler greetingHandler) {
+		return RouterFunctions
+			.route(RequestPredicates.GET("/hola").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), greetingHandler::hola);
+	}
+	
+	@Bean
+	public RouterFunction<ServerResponse> routeMabuhay(GreetingHandler greetingHandler) {
+		return RouterFunctions
+			.route(RequestPredicates.GET("/mabuhay").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), greetingHandler::mabuhay);
+	}
+}
